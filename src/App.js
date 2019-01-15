626ccc01eb959f8dd0a7c68dd4b22e4f2e4f0cb2
Frank Faustino <nawzyah@gmail.com>
2019-01-15 14:04:23 -0800

    ⚡️ Add example code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Link, Switch, Route } from 'react-router-dom'
+
+import Carousel from './Carousel'
+import Test from './Test'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Link to="/">test</Link>{' '}
+        <Link to="/demo">demo</Link>
+        <Switch>
+          <Route exact path="/" component={Test} />
+          <Route path="/demo" component={Carousel} />
+        </Switch>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
